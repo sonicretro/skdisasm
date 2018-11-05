b18391a4bc1f923f4fb72e3ea9eca3ab9d7e0016
Fred Bronze <fredbronze@gmail.com>
2018-11-05 21:09:16 +0000

    Also apply labels to debug lists

--- Levels/LRZ/Debug/Act 1.asm
+++ Levels/LRZ/Debug/Act 1.asm
@@ -1,8 +1,8 @@
 	;          Object				Mappings			Subtype Frame Arttile
-	dbglistobj Obj_Ring,				Map_Ring,			   0,   0, make_art_tile($6BC,1,1)
-	dbglistobj Obj_Monitor,				Map_Monitor,			   6,   0, make_art_tile($4C4,0,0)
-	dbglistobj Obj_PathSwap,			Map_PathSwap,			   9,   1, make_art_tile($6BC,1,0)
-	dbglistobj Obj_PathSwap,			Map_PathSwap,			  $D,   5, make_art_tile($6BC,1,0)
+	dbglistobj Obj_Ring,				Map_Ring,			   0,   0, make_art_tile(ArtTile_Ring,1,1)
+	dbglistobj Obj_Monitor,				Map_Monitor,			   6,   0, make_art_tile(ArtTile_Monitors,0,0)
+	dbglistobj Obj_PathSwap,			Map_PathSwap,			   9,   1, make_art_tile(ArtTile_Ring,1,0)
+	dbglistobj Obj_PathSwap,			Map_PathSwap,			  $D,   5, make_art_tile(ArtTile_Ring,1,0)
 	dbglistobj Obj_Spring,				Map_Spring,			 $81,   0, make_art_tile($4A4,0,0)
 	dbglistobj Obj_Spring,				Map_Spring,			 $90,   3, make_art_tile($4B4,0,0)
 	dbglistobj Obj_Spring,				Map_Spring,			 $A0,   6, make_art_tile($4A4,0,0)
