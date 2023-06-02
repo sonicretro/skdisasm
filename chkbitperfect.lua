f5c23e4449d7788a6c44ad2e2526f2d68e01867a
Clownacy <Clownacy@users.noreply.github.com>
2023-06-02 05:31:05 +0100

    Prevent `chkbitperfect.lua` throwing errors.

--- chkbitperfect.lua
+++ chkbitperfect.lua
@@ -11,30 +11,32 @@ local co
 
 -- Sonic 3
 co = coroutine.create(function() dofile("buildS3.lua") end)
-assert(coroutine.resume(co))
+local _, exit_code_s3 = assert(coroutine.resume(co))
 
 -- Sonic & Knuckles
 co = coroutine.create(function() dofile("buildSK.lua") end)
-assert(coroutine.resume(co))
+local _, exit_code_sk = assert(coroutine.resume(co))
 
 -- Restore os.exit back to normal.
 os.exit = os_exit
 
--- Hash the ROMs.
-local s3_hash = clownmd5.HashFile("s3built.bin")
-local sk_hash = clownmd5.HashFile("skbuilt.bin")
+if exit_code_s3 ~= false and exit_code_sk ~= false then
+	-- Hash the ROMs.
+	local s3_hash = clownmd5.HashFile("s3built.bin")
+	local sk_hash = clownmd5.HashFile("skbuilt.bin")
 
--- Verify the hashes.
-print "-------------------------------------------------------------"
+	-- Verify the hashes.
+	print "-------------------------------------------------------------"
 
-if s3_hash == "\xD7\x24\xEA\x4D\xD4\x17\xFE\x33\x0C\x9D\xCF\xD9\x55\xC5\x96\xB2" then
-	print "Sonic 3 ROM is bit-perfect (with USA version)."
-else
-	print "Sonic 3 ROM is NOT bit-perfect (with USA version)."
-end
+	if s3_hash == "\xD7\x24\xEA\x4D\xD4\x17\xFE\x33\x0C\x9D\xCF\xD9\x55\xC5\x96\xB2" then
+		print "Sonic 3 ROM is bit-perfect (with USA version)."
+	else
+		print "Sonic 3 ROM is NOT bit-perfect (with USA version)."
+	end
 
-if sk_hash == "\x4E\xA4\x93\xEA\x4E\x9F\x6C\x9E\xBF\xCC\xBD\xB1\x51\x10\x36\x7E" then
-	print "Sonic & Knuckles ROM is bit-perfect."
-else
-	print "Sonic & Knuckles ROM is NOT bit-perfect."
+	if sk_hash == "\x4E\xA4\x93\xEA\x4E\x9F\x6C\x9E\xBF\xCC\xBD\xB1\x51\x10\x36\x7E" then
+		print "Sonic & Knuckles ROM is bit-perfect."
+	else
+		print "Sonic & Knuckles ROM is NOT bit-perfect."
+	end
 end
