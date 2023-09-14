704ba169fbedfb8a68c77690fecfc408aff30fe4
Clownacy <Clownacy@users.noreply.github.com>
2023-09-14 22:27:05 +0100

    Make chkbitperfect failure consistent with the other disassemblies.

--- chkbitperfect.lua
+++ chkbitperfect.lua
@@ -31,12 +31,12 @@ if not abort_s3 and not abort_sk then
 	if s3_hash == "\xD7\x24\xEA\x4D\xD4\x17\xFE\x33\x0C\x9D\xCF\xD9\x55\xC5\x96\xB2" then
 		print "Sonic 3 ROM is bit-perfect (with USA version)."
 	else
-		print "Sonic 3 ROM is NOT bit-perfect (with USA version)."
+		print "Sonic 3 ROM is NOT bit-perfect (with USA version)!"
 	end
 
 	if sk_hash == "\x4E\xA4\x93\xEA\x4E\x9F\x6C\x9E\xBF\xCC\xBD\xB1\x51\x10\x36\x7E" then
 		print "Sonic & Knuckles ROM is bit-perfect."
 	else
-		print "Sonic & Knuckles ROM is NOT bit-perfect."
+		print "Sonic & Knuckles ROM is NOT bit-perfect!"
 	end
 end
