07200ad7bbba000a69227a053448a6e06befa06f
Clownacy <Clownacy@users.noreply.github.com>
2025-07-08 23:31:11 +0100

    Update Lua common.
    Smaller build scripts; yay!

--- buildS3.lua
+++ buildS3.lua
@@ -2,19 +2,11 @@
 
 local common = require "build_tools.lua.common"
 
-local exit_code
-
-local message, abort = common.build_rom("s3", "s3built", "", "-p=FF -z=0,uncompressed,Size_of_Snd_driver_guess,before -z=1300,uncompressed,Size_of_Snd_driver2_guess,before", false, "https://github.com/sonicretro/skdisasm")
-
-if message then
-	exit_code = false
-end
-
-if abort then
-	os.exit(exit_code, true)
-end
+-- Build the ROM.
+common.build_rom_and_handle_failure("s3", "s3built", "", "-p=FF -z=0,uncompressed,Size_of_Snd_driver_guess,before -z=1300,uncompressed,Size_of_Snd_driver2_guess,before", false, "https://github.com/sonicretro/skdisasm")
 
 -- Correct the ROM's header with a proper checksum and end-of-ROM value.
 common.fix_header("s3built.bin")
 
-os.exit(exit_code, false)
+-- A successful build; we can quit now.
+common.exit()
--- buildSK.lua
+++ buildSK.lua
@@ -13,20 +13,18 @@ local improved_sound_driver_compression = false
 -- End of settings --
 ---------------------
 
+-------------------------------------
+-- Actual build script begins here --
+-------------------------------------
+
 local common = require "build_tools.lua.common"
 
+-- Build the ROM.
 local compression = improved_sound_driver_compression and "kosinski-optimised" or "kosinski"
-local message, abort = common.build_rom("sonic3k", "skbuilt", "-D Sonic3_Complete=0", "-p=FF -z=0," .. compression .. ",Size_of_Snd_driver_guess,before -z=1300," .. compression .. ",Size_of_Snd_driver2_guess,before", false, "https://github.com/sonicretro/skdisasm")
-
-if message then
-	exit_code = false
-end
-
-if abort then
-	os.exit(exit_code, true)
-end
+common.build_rom_and_handle_failure("sonic3k", "skbuilt", "-D Sonic3_Complete=0", "-p=FF -z=0," .. compression .. ",Size_of_Snd_driver_guess,before -z=1300," .. compression .. ",Size_of_Snd_driver2_guess,before", false, "https://github.com/sonicretro/skdisasm")
 
 -- Correct the ROM's header with a proper checksum and end-of-ROM value.
 common.fix_header("skbuilt.bin")
 
-os.exit(exit_code, false)
+-- A successful build; we can quit now.
+common.exit()
